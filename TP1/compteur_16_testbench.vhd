a452771a03441c73e9035b8d98458ccb6191b85a
LittleBangTheory <gabicho05@gmail.com>
2022-11-13 21:21:22 +0100

    TP1_final

--- TP1/compteur_16_testbench.vhd
+++ TP1/compteur_16_testbench.vhd
@@ -1,5 +1,6 @@
 LIBRARY IEEE;
 use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity test_compteur_16 is
 end test_compteur_16;
@@ -14,31 +15,41 @@ architecture test_compteur of test_compteur_16 is
   --outputs
   signal  deb_tb : STD_LOGIC := '0';
   signal out_count_tb : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
-  signal out_count_i_tb : STD_LOGIC := '0';
 
 begin
 
   tst: entity work.compteur_16(compteur)
-    portmap (clk => clk_tb, rst => rst_tb, enable => enable_tb, deb => deb_tb, out_count => out_count_tb, out_count_i => out_count_i_tb);
+    port map (clk => clk_tb,
+    rst => rst_tb,
+    enable => enable_tb,
+    deb => deb_tb,
+    out_count => out_count_tb);
   
-  clk_tb <= not clk_tb after 5 ms;
+  clk_tb <= not clk_tb after 5 ns;
   
   tst_process: process
   begin
+    
     rst_tb <= '0';
     enable_tb <= '1';
-    wait for 20 ms;
+    wait for 200 ns;
   
-    rst_tb <= '0';
-    enable_tb <= '0';
-    wait for 30 ms;
-
     rst_tb <= '1';
     enable_tb <= '1';
-    wait for 20 ms;
+    wait for 20 ns;
 
-    rst_tb <= '1';
+    rst_tb <= '0';
+    enable_tb <= '1';
+    wait for 50 ns;
+
+    rst_tb <= '0';
     enable_tb <= '0';
-    wait for 30 ms;
+    wait for 40 ns;
+    
+    rst_tb <= '0';
+    enable_tb <= '1';
+    wait for 50 ns;
+    
+    wait;
   end process;  
 end test_compteur;
