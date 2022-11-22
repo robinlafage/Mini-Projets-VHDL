4a779ac0489129f5e282cbedabb909544afa0cb9
LittleBangTheory <gabicho05@gmail.com>
2022-11-22 19:32:48 +0100

    structure chronomètre

--- TP2/chronometre.vhd
+++ TP2/chronometre.vhd
@@ -12,52 +12,77 @@ entity chronometer is
     );
 end chronometer;
 architecture Behavioral of chronometer is
-    begin
-        compteur_clk : entity work.compteur_n
-            generic map(
-                C_NB_BIT_COUNTER => 4,
-                C_MODULO => 10 --On fixe le N à 10 et donc la longueur du compteur à 4
-            )
-            port map(
-                clk => clk,
-                rst => rst,
-                enable => start,
-                max => ,
-                out_count => out_count_tb
-            );
+    signal out_count_clk : STD_LOGIC_VECTOR (25 downto 0);
+    signal max_clk, max_seconds, max_minutes : STD_LOGIC := '0';
+begin
+    compteur_clk : entity work.compteur_n
+        generic map(
+            C_NB_BIT_COUNTER => 26,
+            C_MODULO => 50000000
+        )
+        port map(
+            clk => clk,
+            rst => rst,
+            enable => start,
+            max => max_clk,
+            out_count => out_count_clk
+        );
+    
+    compteur_seconds : entity work.compteur_n
+        generic map(
+            C_NB_BIT_COUNTER => 8,
+            C_MODULO => 60 
+        )
+        port map(
+            clk => clk,
+            rst => rst,
+            enable => start,
+            max => max_seconds,
+            out_count => seconds
+        );
         
-        compteur_seconds : entity work.compteur_n
-            generic map(
-                C_NB_BIT_COUNTER => 4,
-                C_MODULO => 10 --On fixe le N à 10 et donc la longueur du compteur à 4
-            )
-            port map(
-                clk => clk,
-                rst => rst,
-                enable => start,
-                max => max_tb,
-                out_count => out_count_tb
-            );
-            
-        compteur_minutes : entity work.compteur_n
-            generic map(
-                C_NB_BIT_COUNTER => 4,
-                C_MODULO => 10 --On fixe le N à 10 et donc la longueur du compteur à 4
-            )
-            port map(
-                clk => clk,
-                rst => rst,
-                enable => start,
-                max => max_tb,
-                out_count => out_count_tb
-            );
+    compteur_minutes : entity work.compteur_n
+        generic map(
+            C_NB_BIT_COUNTER => 8,
+            C_MODULO => 60 
+        )
+        port map(
+            clk => clk,
+            rst => rst,
+            enable => start,
+            max => max_minutes,
+            out_count => minutes
+        );
+
+    p_chrono : process(clk)
+    begin
+        if (clk'event and clk = '1') then
+			if (rst = '1') then --reset synchrone
+				out_count_clk <= (others => '0');
+                minutes <= '0';
+                seconds <= '0';
+			elsif (start = '1') then
+				if (max_clk = 1) then 
+					--incrÃ©menter secondes
+				elsif (max_seconds = 1) then 
+				    --incrÃ©menter minutes
+				elsif (max_minutes = 1) then
+                    rst <= '1';
+                else
+					out_count_clk <= out_count_clk+1; --si pas de debordement on compte normalement
+				end if;
+			end if;
+		end if;
+	end process;
+
+        
     
 
 end Behavioral;
 
 
-signal out_seconds : UNSIGNED (7 downto 0);
-    signal out_minutes : UNSIGNED (7 downto 0);
+--signal out_seconds : UNSIGNED (7 downto 0);
+--signal out_minutes : UNSIGNED (7 downto 0);
     
 --    begin
 --        seconds <= STD_LOGIC_VECTOR(out_seconds);
@@ -77,8 +102,13 @@ signal out_seconds : UNSIGNED (7 downto 0);
 --                        out_seconds <= (others => '0');
 --                        out_minutes <= out_minutes + 1;
 --                    else
---                        out_seconds <= out_seconds + 1; --si pas de débordement on compte normalement
+--                        out_seconds <= out_seconds + 1; --si pas de dï¿½bordement on compte normalement
 --                    end if;
 --                end if;
 --            end if;
 --        end process;
+
+
+-- 26 bits pour coder 50Mhz, 6 bits pour coder 60 secondes, 6 bits pour coder 60 minutes, 3 bits pour coder les 3 max
+-- 41 bits donc 41 bascules 
+-- Secondes et Minutes sont fixÃ©s sur 8 bits dans l'entity, Ã©trange 
