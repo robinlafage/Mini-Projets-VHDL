5e566ec4e91f964de2426c3def4089a34eccdcd8
LittleBangTheory <gabicho05@gmail.com>
2022-11-29 16:35:23 +0100

    chronometer and testbench update

--- TP2/chronometre.vhd
+++ TP2/chronometre.vhd
@@ -13,9 +13,15 @@ entity chronometer is
 end chronometer;
 
 architecture Behavioral of chronometer is
+    --define 2 unsused signals, the out of the clock counter, and the max of the minutes :
     signal out_count_clk : STD_LOGIC_VECTOR (25 downto 0);
-    signal max_clk, max_seconds, max_minutes, start_seconds, start_minutes : STD_LOGIC := '0';
+    signal max_minutes : STD_LOGIC;
+
+    --define the 3 internal signals : 
+    signal max_clk, max_seconds, start_minutes : STD_LOGIC := '0';
+    
     begin
+        --define the clock counter :
         compteur_clk : entity work.compteur_n
             generic map(
                 C_NB_BIT_COUNTER => 26,
@@ -28,7 +34,7 @@ architecture Behavioral of chronometer is
                 max => max_clk,
                 out_count => out_count_clk
             );
-    
+        --define the seconds counter :
         compteur_seconds : entity work.compteur_n
             generic map(
                 C_NB_BIT_COUNTER => 8,
@@ -37,11 +43,11 @@ architecture Behavioral of chronometer is
             port map(
                 clk => clk,
                 rst => rst,
-                enable => start_seconds,
+                enable => max_clk,
                 max => max_seconds,
                 out_count => seconds
             );
-    
+        --define the minutes counter :
         compteur_minutes : entity work.compteur_n
             generic map(
                 C_NB_BIT_COUNTER => 8,
@@ -54,62 +60,6 @@ architecture Behavioral of chronometer is
                 max => max_minutes,
                 out_count => minutes
             );
-    
-        p_chrono : process(clk)
-        begin
-            if (clk'event and clk = '1') then
-                start_seconds <= '0';
-                start_minutes <= '0';
-                if (rst = '1') then --reset synchrone
-                    out_count_clk <= (others => '0');
-                    minutes (others => '0');
-                    seconds (others => '0');
-                elsif (start = '1') then
-                    if (max_clk = 1) then 
-                        start_seconds <= '1';
-                    end if
-                    if (max_seconds = 1) then 
-                        start_minutes <= '1';
-                    end if
-                    if (max_minutes = 1) then
-                        rst <= '1';
-                    else
-                        --clk censé compter seul, mais sinon : out_count_clk <= out_count_clk+1;
-                    end if;
-                end if;
-            end if;
-        end process;
+        --when the max of the seconds and the max of the clock are 1, the max of the minutes is 1 :
+        start_minutes <= max_seconds and max_clk;
 end Behavioral;
-
-
---signal out_seconds : UNSIGNED (7 downto 0);
---signal out_minutes : UNSIGNED (7 downto 0);
-    
---    begin
---        seconds <= STD_LOGIC_VECTOR(out_seconds);
---        minutes <= STD_LOGIC_VECTOR(out_minutes);
-        
---        p1 : process(clk)
---        begin
---            if (clk'event and clk = '1') then
---                if (rst = '1') then --reset synchrone
---                    out_seconds <= (others => '0');
---                    out_minutes <= (others => '0');
---                elsif (start = '1') then
---                    if (out_minutes = 59 and out_seconds = 59) then --cas debordement minutes
---                        out_seconds <= (others => '0');
---                        out_minutes <= (others => '0');
---                    elsif (out_seconds = 59) then --cas debordement seconds
---                        out_seconds <= (others => '0');
---                        out_minutes <= out_minutes + 1;
---                    else
---                        out_seconds <= out_seconds + 1; --si pas de d�bordement on compte normalement
---                    end if;
---                end if;
---            end if;
---        end process;
-
-
--- 26 bits pour coder 50Mhz, 6 bits pour coder 60 secondes, 6 bits pour coder 60 minutes, 3 bits pour coder les 3 max
--- 41 bits donc 41 bascules 
--- Secondes et Minutes sont fixés sur 8 bits dans l'entity, étrange 
--- TP2/tb_chronometre.vhd
+++ TP2/tb_chronometre.vhd
@@ -19,8 +19,10 @@ begin
 	minutes => minutes_tb,
 	seconds => seconds_tb
 	);
-
-	clk_tb <= not(clk_tb) after 5 ns;
-	start_tb <= '1', '0' after 200ns, '1' after 250ns;
-	rst_tb <= '1', '0' after 10 ns, '1' after 80 ns, '0' after 100ns;
+	--change of state every 10ns, so a clock period of 20ns, and 20ns*50 000 000 = 1s
+	clk_tb <= not(clk_tb) after 10ns;
+	--stop the chronometer for 5s
+	start_tb <= '1', '0' after 100000ms, '1' after 150000ms;
+	--reset the chronometer during 5s
+	rst_tb <= '0', '1' after 400000ms, '0' after 450000ms;
 end tb;
